ccb6004812152eaa95d47849ef16593526a8d92d
“anmoltanwar” <anmol.tanwar@geta.ai>
2024-02-04 21:00:53 +0530

    first commit

---
+++ lib/utils.ts
@@ -0,0 +1,141 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+import qs from "query-string";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const formatDateTime = (dateString: Date) => {
+  const dateTimeOptions: Intl.DateTimeFormatOptions = {
+    weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+    month: "short", // abbreviated month name (e.g., 'Oct')
+    day: "numeric", // numeric day of the month (e.g., '25')
+    hour: "numeric", // numeric hour (e.g., '8')
+    minute: "numeric", // numeric minute (e.g., '30')
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+  };
+
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+    month: "short", // abbreviated month name (e.g., 'Oct')
+    year: "numeric", // numeric year (e.g., '2023')
+    day: "numeric", // numeric day of the month (e.g., '25')
+  };
+
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: "numeric", // numeric hour (e.g., '8')
+    minute: "numeric", // numeric minute (e.g., '30')
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+  };
+
+  const formattedDateTime: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateTimeOptions
+  );
+
+  const formattedDate: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateOptions
+  );
+
+  const formattedTime: string = new Date(dateString).toLocaleString(
+    "en-US",
+    timeOptions
+  );
+
+  return {
+    dateTime: formattedDateTime,
+    dateOnly: formattedDate,
+    timeOnly: formattedTime,
+  };
+};
+
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+
+// export const formatPrice = (price: string) => {
+//   const amount = parseFloat(price);
+//   const formattedPrice = new Intl.NumberFormat("en-US", {
+//     style: "currency",
+//     currency: "USD",
+//   }).format(amount);
+
+//   return formattedPrice;
+// };
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: 'INR' | 'USD' | 'EUR' | 'GBP' | 'BDT'
+    notation?: Intl.NumberFormatOptions['notation']
+  } = {}
+) {
+  const { currency = 'INR', notation = 'compact' } = options
+
+  const numericPrice =
+    typeof price === 'string' ? parseFloat(price) : price
+
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice)
+}
+
+export function formUrlQuery({ params, key, value }: UrlQueryParams) {
+  const currentUrl = qs.parse(params);
+
+  currentUrl[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
+}
+
+export function removeKeysFromQuery({
+  params,
+  keysToRemove,
+}: RemoveUrlQueryParams) {
+  const currentUrl = qs.parse(params);
+
+  keysToRemove.forEach((key: string) => {
+    delete currentUrl[key];
+  });
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
+}
+
+export const handleError = (error: unknown) => {
+  console.error({ error });
+  throw new Error(typeof error === "string" ? error : JSON.stringify(error));
+};
+
+// // Function to generate a JWT token
+// export async function jwtToken(userId: string) {
+//   // Generate a JWT
+//   const token = jwt.sign({ userId: userId }, CONFIG.JWT_SECRET_KEY);
+//   return token;
+// }
+
+// // Function to decode a JWT token
+// export async function decodeJwtToken(token: string) {
+//   try {
+//     // Decode the token
+//     const decoded = jwt.verify(token, CONFIG.JWT_SECRET_KEY);
+//     return decoded;
+//   } catch (error) {
+//     console.error(`Error decoding JWT: ${error}`);
+//     throw error;
+//   }
+// }
